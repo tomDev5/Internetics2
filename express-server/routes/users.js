42deedc002de9da4e935c7be8ab32cbfbd4e5824
tomDev5 <tom@lubin.ws>
2020-12-12 16:04:08 +0200

    everything else

--- express-server/routes/users.js
+++ express-server/routes/users.js
@@ -17,6 +17,34 @@ MongoClient.connect(connectionString, (err, client) => {
             });
     });
 
+    router.post('/login', async (req,res) => {
+        if(await usersDB.findOne({_id: req.body.username, password: req.body.password})){
+            //if found user
+            //add session
+        }else{
+            //if not found
+            res.sendStatus(401)
+        }
+    });
+
+    router.post('/signup', async (req,res) => {
+        body = req.body
+        //content checks
+        if(body.username === '' || body.name === '' || body.password === '' || body.password !== body.password2){
+            res.sendStatus(406)
+        }else if(!await usersDB.findOne({_id: body.username})){
+            //if username unique
+            //create document:
+            usersDB.insertOne({_id: body.username, name: body.name, password: body.password})
+            res.sendStatus(200)
+        }else{
+            //username taken
+            res.sendStatus(403)
+        }
+        
+        res.end()
+    });
+
 })
  
 module.exports = router;
--- express-server/server.js
+++ express-server/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const session = require('express-session')
 const mongoose = require('mongoose');
 const users = require('./routes/users')
+var bodyParser = require('body-parser')
 
+const cors = require('cors');
 const app = express();
+
+app.use(cors())
+app.use( bodyParser.json() )
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}))
 
 app.post("/",(req,res) => {
   res.send('post')
