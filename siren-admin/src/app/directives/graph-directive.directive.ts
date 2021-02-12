6460bb5d9e7cfa5ae94e6f0a3ad0405b83097328
Omer Lubin <omer@lubin.ws>
2021-02-12 16:05:20 +0200

    graph fix

--- siren-admin/src/app/directives/graph-directive.directive.ts
+++ siren-admin/src/app/directives/graph-directive.directive.ts
@@ -2,7 +2,6 @@ import { Content } from '@angular/compiler/src/render3/r3_ast';
 import { Directive, ElementRef, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { DataPoint } from '../models/DataPoint';
 import * as d3 from 'd3';
-import { drag } from 'd3';
 
 @Directive({
   selector: '[myGraph]'
@@ -25,7 +24,6 @@ export class GraphDirectiveDirective implements OnDestroy {
         this.ref.nativeElement.innerHTML = ''
         this.initializeChart()
         this.drawn = true
-        console.log(this.graphPoints)
       }
     }
   }
@@ -35,70 +33,62 @@ export class GraphDirectiveDirective implements OnDestroy {
   }
 
   private initializeChart(){
-    //WORKS
-       
-  var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 700 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
-  
-  var parseDate = d3.time.format("%Y-%m-%d").parse;
-  
-  
-  var x = d3.time.scale()
-      .range([0, width])
-  
-  var y = d3.scale.linear()
-      .range([height, 0]);
-  
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-  
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
-  
-  var line = d3.svg.line()
-      .x((d:any) => { return x(d.date); })
-      .y((d:any) => { return y(d.close); });
-  
-  var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .attr("fill", "none")
-      .attr("stroke", "#000");
-  
-    var data = this.graphPoints.map(function(d) {
-        return {
-           date: new Date(d[0]),
-           close: d[1]
-        };
-        
-    });
-  
-    x.domain(d3.extent(data, (d) => { return d.date; }));
-    y.domain(d3.extent(data, (d):any => { return d.close; }));
-  
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-  
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Number of total posts");
-  
-    svg.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line);  
+        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+
+    var x = d3.time.scale()
+        .range([0, width])
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function(d:any) { return x(d.date); })
+        .y(function(d:any) { return y(d.close); });
+
+    var svg = d3.select(this.ref.nativeElement).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var data = this.graphPoints.map(function(d) {
+          return {
+            date: new Date(d[0]),
+            close: d[1]
+          };
+          
+      });
+
+      x.domain(d3.extent(data, function(d) { return d.date; }));
+      y.domain(d3.extent(data, function(d) { return d.close; }));
+
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .style("fill", "none")
+          .style("stroke", "black")
+          .call(xAxis);
+
+      svg.append("g")
+          .style("fill", "none")
+          .style("stroke", "black")
+          .call(yAxis);
+
+      svg.append("path")
+          .datum(data)
+          .attr("d", line)
+          .style("fill", "none")
+          .style("stroke", "steelblue")
+          .style("stroke-width", "1.5px");
   }
 }
